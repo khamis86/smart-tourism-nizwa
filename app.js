ce98883bd9ebcc216b9bda294a275344df653006
khamis86 <69185769+khamis86@users.noreply.github.com>
2026-02-27 01:51:56 +0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
